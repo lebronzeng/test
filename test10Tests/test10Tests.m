67f154a23ee1b988fe280628881d7e90d7c853ee
lebronzeng <745810636@qq.com>
2015-01-17 15:12:14 +0800

    Initial Commit

---
+++ test10Tests/test10Tests.m
@@ -0,0 +1,34 @@
+//
+//  test10Tests.m
+//  test10Tests
+//
+//  Created by qianfeng on 15-1-17.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test10Tests : XCTestCase
+
+@end
+
+@implementation test10Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
